27e36ea4a652cf20652238d99d13076231665483
xavier <xavierbertomeuplatas@gmail.com>
2025-02-20 12:33:51 +0100

    CameraX test 2 - Switch Camera

--- app/src/main/java/com/maxlift/presentation/ui/activities/CameraPreviewScreen.kt
+++ app/src/main/java/com/maxlift/presentation/ui/activities/CameraPreviewScreen.kt
@@ -9,12 +9,20 @@ import androidx.camera.core.ImageCaptureException
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.view.PreviewView
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -24,30 +32,54 @@ import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.content.ContextCompat
 import kotlin.coroutines.resume
 import kotlin.coroutines.suspendCoroutine
+import androidx.compose.material.icons.filled.Refresh
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun CameraPreviewScreen() {
-    val lensFacing = CameraSelector.LENS_FACING_BACK
-    val lifecycleOwner = LocalLifecycleOwner.current
     val context = LocalContext.current
-    val preview = Preview.Builder().build()
-    val previewView = remember {
-        PreviewView(context)
-    }
-    val cameraxSelector = CameraSelector.Builder().requireLensFacing(lensFacing).build()
-    val imageCapture = remember {
-        ImageCapture.Builder().build()
-    }
-    LaunchedEffect(lensFacing) {
+    val lifecycleOwner = LocalLifecycleOwner.current
+    val lensFacing = remember { mutableIntStateOf(CameraSelector.LENS_FACING_BACK) }
+
+    val previewView = remember { PreviewView(context) }
+    val imageCapture = remember { ImageCapture.Builder().build() }
+
+    LaunchedEffect(lensFacing.intValue) {
         val cameraProvider = context.getCameraProvider()
+        val cameraSelector = CameraSelector.Builder().requireLensFacing(lensFacing.intValue).build()
+
         cameraProvider.unbindAll()
-        cameraProvider.bindToLifecycle(lifecycleOwner, cameraxSelector, preview, imageCapture)
-        preview.surfaceProvider = previewView.surfaceProvider
+        cameraProvider.bindToLifecycle(
+            lifecycleOwner,
+            cameraSelector,
+            Preview.Builder().build().also { it.surfaceProvider = previewView.surfaceProvider },
+            imageCapture
+        )
     }
-    Box(contentAlignment = Alignment.BottomCenter, modifier = Modifier.fillMaxSize()) {
-        AndroidView({ previewView }, modifier = Modifier.fillMaxSize())
-        Button(onClick = { captureImage(imageCapture, context) }) {
-            Text(text = "Capture Image")
+
+    AndroidView({ previewView }, modifier = Modifier.fillMaxSize())
+
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.BottomCenter) {
+        Button(modifier = Modifier.padding(16.dp), onClick = { captureImage(imageCapture, context) }) {
+            Text(
+                text = "Capture Image"
+            )
+        }
+    }
+
+    Box(modifier = Modifier.fillMaxSize().padding(16.dp), contentAlignment = Alignment.BottomEnd) {
+        Button(onClick = {
+            lensFacing.intValue = if (lensFacing.intValue == CameraSelector.LENS_FACING_BACK) {
+                CameraSelector.LENS_FACING_FRONT
+            } else {
+                CameraSelector.LENS_FACING_BACK
+            }
+        }) {
+            Icon(
+                imageVector = Icons.Default.Refresh,
+                contentDescription = "Switch Camera",
+                modifier = Modifier.size(24.dp)
+            )
         }
     }
 }
@@ -62,11 +94,11 @@ private suspend fun Context.getCameraProvider(): ProcessCameraProvider =
     }
 
 private fun captureImage(imageCapture: ImageCapture, context: Context) {
-    val name = "CameraxImage.jpeg"
+    val name = "Image.jpeg"
     val contentValues = ContentValues().apply {
         put(MediaStore.MediaColumns.DISPLAY_NAME, name)
         put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg")
-        put(MediaStore.Images.Media.RELATIVE_PATH, "Pictures/CameraX-Image")
+        put(MediaStore.Images.Media.RELATIVE_PATH, "Pictures/MaxLift")
     }
     val outputOptions = ImageCapture.OutputFileOptions
         .Builder(
@@ -80,7 +112,7 @@ private fun captureImage(imageCapture: ImageCapture, context: Context) {
         ContextCompat.getMainExecutor(context),
         object : ImageCapture.OnImageSavedCallback {
             override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
-                println("Successs")
+                println("Success")
             }
 
             override fun onError(exception: ImageCaptureException) {
--- app/src/main/java/com/maxlift/presentation/ui/activities/MainActivity.kt
+++ app/src/main/java/com/maxlift/presentation/ui/activities/MainActivity.kt
@@ -10,10 +10,7 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.content.ContextCompat
 import com.maxlift.presentation.theme.MaxLiftTheme
 
