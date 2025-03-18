d91a26b5a7bf854d590bfc3f2d2efa96c98d86f8
xavier <xavierbertomeuplatas@gmail.com>
2025-03-18 20:35:27 +0100

    SFX + Haptics + Animation (pijada)

---
+++ app/src/main/java/com/maxlift/presentation/ui/common/RecordButton.kt
@@ -0,0 +1,81 @@
+package com.maxlift.presentation.ui.common
+
+import android.content.Context
+import android.media.AudioManager
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.spring
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalView
+import androidx.compose.ui.unit.dp
+import androidx.core.view.HapticFeedbackConstantsCompat
+
+@Composable
+fun RecordButton(size: Int, onClick: () -> Unit) {
+    var isClicked by remember { mutableStateOf(false) }
+    val context = LocalContext.current
+    val audioManager = remember { context.getSystemService(Context.AUDIO_SERVICE) as AudioManager }
+    val view = LocalView.current
+
+    val scale by animateFloatAsState(
+        targetValue = if (isClicked) 0.8f else 1f,
+        animationSpec = spring(
+            dampingRatio = Spring.DampingRatioMediumBouncy,
+            stiffness = Spring.StiffnessLow
+        ),
+        label = "scale"
+    )
+
+    Box(
+        modifier = Modifier
+            .size(size.dp)
+            .clickable {
+                onClick()
+                isClicked = !isClicked
+                audioManager.playSoundEffect(AudioManager.FX_KEY_CLICK, 0.6f)
+                view.performHapticFeedback(HapticFeedbackConstantsCompat.VIRTUAL_KEY)
+            },
+        contentAlignment = Alignment.Center,
+    ) {
+
+        Box(
+            modifier = Modifier
+                .size(size.dp)
+                .clip(CircleShape)
+                .background(color = MaterialTheme.colorScheme.primaryContainer)
+        )
+
+        Box(
+            modifier = Modifier
+                .size((size*0.90).dp)
+                .clip(CircleShape)
+                .background(Color.Black)
+        )
+
+        Box(
+            modifier = Modifier
+                .size((size*0.90).dp)
+                .scale(scale)
+                .clip(CircleShape)
+                .border((size*0.03).dp, Color.Black, CircleShape)
+                .background(if(isClicked) Color.Red else MaterialTheme.colorScheme.primaryContainer)
+        )
+    }
+}
--- app/src/main/java/com/maxlift/presentation/ui/common/Utils.kt
+++ app/src/main/java/com/maxlift/presentation/ui/common/Utils.kt
@@ -5,12 +5,6 @@ import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.widget.Toast
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
@@ -18,7 +12,6 @@ import androidx.compose.material.icons.filled.Visibility
 import androidx.compose.material.icons.filled.VisibilityOff
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -26,15 +19,10 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
-import androidx.compose.ui.unit.dp
 
 @Composable
 fun PasswordTextField(
@@ -62,37 +50,6 @@ fun PasswordTextField(
             onDone = { keyboardController?.hide() }
         )
     )
-}
-
-@Composable
-fun RecordButton(size: Int, onClick: () -> Unit) {
-    var isClicked by remember { mutableStateOf(false) }
-
-    Box(
-        modifier = Modifier
-            .size(size.dp)
-            .clickable {
-                onClick()
-                isClicked = !isClicked
-            },
-        contentAlignment = Alignment.Center,
-    ) {
-
-        Box(
-            modifier = Modifier
-                .size(size.dp)
-                .clip(CircleShape)
-                .background(MaterialTheme.colorScheme.primaryContainer)
-        )
-
-        Box(
-            modifier = Modifier
-                .size((size*0.90).dp)
-                .clip(CircleShape)
-                .border((size*0.03).dp, Color.Black, CircleShape)
-                .background(if(isClicked) Color.Red else MaterialTheme.colorScheme.primaryContainer)
-        )
-    }
 }
 
 fun openWebPage(context: Context, url: String) {
