30f68500ac4504eb37a9708cd1b46fc892e4748f
xavier <xavierbertomeuplatas@gmail.com>
2025-04-01 11:16:35 +0200

    Camera Permission Pseudo Arreglat - Ara si no tens permisos i no tels deixa demanar t'envia a settings. Si no, estas papejat.

--- app/src/main/java/com/maxlift/presentation/ui/MainActivity.kt
+++ app/src/main/java/com/maxlift/presentation/ui/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.maxlift.presentation.ui
 
 import android.Manifest
+import android.app.Activity
+import android.app.AlertDialog
 import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.provider.Settings
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -27,8 +32,6 @@ import com.maxlift.domain.usecase.login.LoginUseCase
 import com.maxlift.domain.usecase.login.LogoutUseCase
 import com.maxlift.domain.usecase.register.RegisterUseCase
 import com.maxlift.presentation.ui.common.MyScaffoldTopAppBar
-import com.maxlift.presentation.ui.feature.calculator.RMForm
-import com.maxlift.presentation.ui.feature.calculator.RMViewModel
 import com.maxlift.presentation.ui.feature.calculator.ResultScreen
 import com.maxlift.presentation.ui.feature.camera.CameraViewModel
 import com.maxlift.presentation.ui.feature.camera.MLKitObjectDetectionScreen
@@ -45,15 +48,15 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-
-        if(!checkPermissions(this)) {
-            ActivityCompat.requestPermissions(
-                this, arrayOf("Manifest.permissions.CAMERA"), 0
-            )
-        }
-
         setContent {
             MyApp()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if(permissionDenied(this)) {
+            doPermissionRequest(this, packageName)
         }
     }
 }
@@ -83,7 +86,6 @@ fun MyApp() {
             composable("register") { UserRegisterForm(RegisterUseCase(userRepository), navController) }
             composable("camera") { TFLiteObjectDetectionScreen() }
             composable("profile") { ProfileScreen(UserViewModel(GetLoggedUserUseCase(userRepository))) }
-            composable("calculator") { RMForm(RMViewModel(), navController) }
             composable("result") { ResultScreen(sharedViewModel) }
             composable("mlkit") { MLKitObjectDetectionScreen(sharedViewModel, navController) }
             composable("persons") { PersonListScreen(PersonViewModel()) }
@@ -91,8 +93,29 @@ fun MyApp() {
     }
 }
 
-private fun checkPermissions(context: Context): Boolean {
+fun permissionDenied(context: Context): Boolean {
     return (ContextCompat.checkSelfPermission(context, Manifest.permission.CAMERA)
         == PackageManager.PERMISSION_DENIED)
+}
+
+fun doPermissionRequest(activity: Activity, packageName: String) {
+    if (ActivityCompat.shouldShowRequestPermissionRationale(
+            activity, "Manifest.permission.CAMERA"))
+    {
+        ActivityCompat.requestPermissions(
+            activity, arrayOf("Manifest.permissions.CAMERA"), 0
+        )
+    } else {
+        AlertDialog.Builder(activity)
+            .setTitle("Camera Permission Required")
+            .setMessage("This app needs access to the camera. Please grant permission.")
+            .setPositiveButton("Go to Settings") { _, _ ->
+                val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                intent.data = Uri.parse("package:$packageName")
+                activity.startActivity(intent)
+            }
+            .create()
+            .show()
+    }
 }
 
